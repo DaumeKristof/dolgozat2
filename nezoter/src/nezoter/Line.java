f9bc2bba81b60f67ba620fbc06e8ade344b40926
Stofkovics <dkris2000@gmail.com>
2019-01-12 12:20:17 +0100

    ...

---
+++ nezoter/src/nezoter/Line.java
@@ -0,0 +1,15 @@
+package nezoter;
+
+public class Line {
+	
+	private String[] sor = new String[20];
+	
+	public Line(String s) {
+		this.sor = s.split("(?!^)");
+	}
+
+	public String[] getSor() {
+		return sor;
+	}
+
+}
